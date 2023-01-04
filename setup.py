fbbb7e6dd832b6991c516fd3136db73641e256da
Disturbo <nottodisturb.disturbo@gmail.com>
2023-01-04 02:08:17 +0100

    Add MIT license, move project to `pyproject.toml` config

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup
+import setuptools
 
-setup(
+if __name__ == "__main__":
+    setuptools.setup()
+
+"""setup(
     name='VersionUtils',
     version='1.1.0',
     author='Disturbo',
     packages=['versionutils'],
     description='Tool to obtain version data for VALORANT',
-)
+)"""
